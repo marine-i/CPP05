d7ed1de3752784ea65af02d833af3f731be93a95
Marine <marine.iguna@gmail.com>
2026-07-17 17:58:58 +0200

    add tests exo00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,17 +6,25 @@ int	main(void)
 	{
 		// Bureaucrat a("A", -1);
 		// Bureaucrat b("B", 151);
-		Bureaucrat c("C", 150);
+	
+		// Bureaucrat c("C", 150);
+		// std::cout << c;
+		// c.incrementGrade();
+		// std::cout << c;
+		// c.decrementGrade();
+		// std::cout << c;
+		// c.decrementGrade();
 
-		// std::cout << a;
-		// std::cout << b;
+		// Bureaucrat d("D", 1);
+		// std::cout << d;
+		// d.decrementGrade();
+		// std::cout << d;
+		// d.incrementGrade();
+		// std::cout << d;
+		// d.incrementGrade();
+		// std::cout << d;
+		// d.decrementGrade();
 
-		std::cout << c;
-		c.incrementGrade();
-		std::cout << c;
-		c.decrementGrade();
-		std::cout << c;
-		c.decrementGrade();
 	}
 	catch (std::exception& e)
 	{
